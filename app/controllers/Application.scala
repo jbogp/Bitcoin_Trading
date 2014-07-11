d43bc3dbc835f534fe1c21994242686a35a6f8f6
Jean-Baptiste Pettit <jbpettit@vanilla.ebi.ac.uk>
2014-07-11 17:25:22 +0100

    Fri

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,22 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import com.tindr.pusher.Pusher
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Mon premier changement"))
-  }
+	def index = Action {
+		Ok(views.html.index("Mon premier changement"))
+	}
+
+	def trades = Action {
+		Ok(views.html.trades("Bitstamp live trades"))
+	}
+
+
+	val pusher = Pusher()
+	println("Application started")
+
+
 
 }
